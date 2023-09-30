130eea78fdc3ef55b08f33da2b9abc88b161b2f9
Jose-Vela <joseeloyvo@gmail.com>
2023-09-29 23:59:22 -0600

    Comenzando a implementar el Patrón de Diseño MVVM
    - Agregamos las data class correspondientes a la data enviada (CompletionData) y a la respuesta recibida (CompletionResponse) de OpenAI.

--- app/src/main/java/com/example/gptapp/Entity.kt
+++ app/src/main/java/com/example/gptapp/Entity.kt
@@ -1,4 +1,33 @@
 package com.example.gptapp
 
-class Entity {
-}
+data class CompletionData(
+    val messages: List<Message>,
+    val model: String,
+    val temperature: Double
+)
+
+data class Message(
+    val content: String,
+    val role: String
+)
+
+data class CompletionResponse(
+    val choices: List<Choice>,
+    val created: Int,
+    val id: String,
+    val model: String,
+    val `object`: String,
+    val usage: Usage
+)
+
+data class Choice(
+    val finish_reason: String,
+    val index: Int,
+    val message: Message
+)
+
+data class Usage(
+    val completion_tokens: Int,
+    val prompt_tokens: Int,
+    val total_tokens: Int
+)
